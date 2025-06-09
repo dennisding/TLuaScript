47a6fcc21ffa4f81dcd41755b85afebe5f08efa1
dennisding <evilangel2000@163.com>
2025-06-09 08:31:08 +0800

    refactor the class plugin system

--- init.lua
+++ init.lua
@@ -3,14 +3,17 @@ require('lua')
 require('unreal')
 require('game')
 
+-- call by engine
 function init()
 	print(_text("hello unreal!!!"))
 end
 
+-- call by engine
 function game_start()
 	print(_text('game start'))
 end
 
+-- call by engine
 function game_exit()
 	_gui:on_game_exit()
 	print(_text('game exist'))
